7d56d10b7ad9fea93c8f1993057c4c19f7d1097b
tiger3132 <32318048+tiger3132@users.noreply.github.com>
2023-11-22 14:39:43 +0100

    Update fetch_data.dart

--- Exercise-2/digital_health/lib/fetch_data.dart
+++ Exercise-2/digital_health/lib/fetch_data.dart
@@ -1,13 +1,21 @@
 import 'package:googleapis/fitness/v1.dart' as fitness;
 import 'package:googleapis_auth/auth.dart';
 
-Future<List<fitness.Session>> fetchData(AutoRefreshingAuthClient client) async {
+Future<List<fitness.DataPoint>> fetchData(AutoRefreshingAuthClient client) async {
   try {
     var data = await fitness.FitnessApi(client).users.sessions.list(
       'me',
-      activityType: [72], // 72 corresponds to sleep in Google Fit
+      activityType: [7], // 72 corresponds to sleep in Google Fit
     );
-    return data.session ?? [];
+    final DateTime endTime = DateTime.now();
+    final DateTime startTime = endTime.subtract(Duration(days: 7));
+    final fitness.Dataset dataset = await fitness.FitnessApi(client).users.dataSources.datasets.get(
+      'me',
+      'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps',
+      // '${1668715722000 * 1000000}-${1700251722000 * 1000000}',
+      '${startTime.millisecondsSinceEpoch * 1000000}-${endTime.millisecondsSinceEpoch * 1000000}'
+    );
+    return dataset.point ?? [];
   } catch (error) {
     print("Error fetching data: $error");
     return [];
